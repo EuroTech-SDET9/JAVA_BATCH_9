b47f03b9628b84c218dd59bd955e4e15374b2e70
Talha <talha43gs@gmail.com>
2023-03-24 21:08:18 +0000

    Day 9 uploaded

---
+++ src/days/day9/ReviewWeek1_2.java
@@ -0,0 +1,52 @@
+package days.day9;
+
+public class ReviewWeek1_2 {
+
+    public static void main(String[] args) {
+
+        String test = "value";
+
+//        test.
+
+        // '1' , '#' , 'L'
+
+        int value = 'L';
+        System.out.println(value < 65 || value > 90);  // if it is true then I'm sure that it is not CAP LETTER!!!
+
+        System.out.println("Char is cap letter ? = " + (value >= 65 && value <= 90));
+
+
+
+        ////
+
+
+        int n3 =3 , n1=1;
+
+        n3 = n3 - n1 +1;  // n3 will be 3
+        System.out.println("n3 = " + n3);
+
+        n3 =3;
+        n3 -= n1 + 1 ;   // n3 will be 1 , they are not same with line 25!!
+        System.out.println("n3 = " + n3);
+
+        //////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
